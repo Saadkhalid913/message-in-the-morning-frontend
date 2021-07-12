fdfa1cc2a8cc6a18bb2848c3f56d088906a4bef7
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-07-11 21:26:53 -0400

    added light and dark mode

--- src/Components/themes.js
+++ src/Components/themes.js
@@ -1,32 +1,45 @@
 const themes = [
+    {"--message-background": "#FFFFFF",
+    "--background": "#F0F0F0",
+    "--text-color": "#000000",
+    "--shadow-color": "#000000",
+     "name": "theme1"},
+
     {"--message-background": "#748CAB",
     "--background": "#1D2D44",
     "--text-color": "#F0EBD8",
     "--shadow-color": "#000000",
-     "name": "theme1"},
+     "name": "theme2"},
+
     {"--message-background": "#7D9E94",
     "--background": "#A7D9A0",
     "--text-color": "#ECF5E9",
     "--shadow-color": "#000000",
-     "name": "theme1"},
+     "name": "theme3"},
 
     {"--message-background": "#6984FA",
     "--background": "#072BAD",
     "--text-color": "#E1E7F5",
     "--shadow-color": "#000000",
-     "name": "theme1"},
+     "name": "theme4"},
 
     {"--message-background": "#690896",
     "--background": "#6B0382",
     "--text-color": "#CBD7F7",
     "--shadow-color": "#210E36",
-     "name": "theme1"},
+     "name": "theme5"},
 
     {"--message-background": "#02BF77",
     "--background": "#97E6D8",
     "--text-color": "#DDEDEC",
     "--shadow-color": "#000000",
-     "name": "theme1"},
+     "name": "theme6"},
+
+    {"--message-background": "#1f1f1f",
+    "--background": "#2D2D2D",
+    "--text-color": "#E0E0E0",
+    "--shadow-color": "#000000",
+     "name": "theme7"}
 
 
 ]
