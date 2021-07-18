5e66e3fc11c269997089059f0afe8a270640c18d
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-07-17 23:28:39 -0400

    added themes

--- src/Components/themes.js
+++ src/Components/themes.js
@@ -5,41 +5,43 @@ const themes = [
     "--shadow-color": "#000000",
      "name": "theme1"},
 
-    {"--message-background": "#748CAB",
-    "--background": "#1D2D44",
-    "--text-color": "#F0EBD8",
-    "--shadow-color": "#000000",
+     
+     {"--message-background": "#BA0D41",
+     "--background": "#F21D5D",
+     "--text-color": "#FFFFFF",
+     "--shadow-color": "#0e0e0e",
      "name": "theme2"},
+     
+     {"--message-background": "#F08000",
+     "--background": "#ED8A18",
+     "--text-color": "#030303",
+     "--shadow-color": "#000000",
+      "name": "theme12"},
 
-    {"--message-background": "#7D9E94",
-    "--background": "#A7D9A0",
-    "--text-color": "#ECF5E9",
-    "--shadow-color": "#000000",
-     "name": "theme3"},
+     {"--message-background": "#F2C00A",
+     "--background": "#FCC80D",
+     "--text-color": "#030303",
+     "--shadow-color": "#000000",
+      "name": "theme12"},
 
-    {"--message-background": "#6984FA",
-    "--background": "#072BAD",
-    "--text-color": "#E1E7F5",
-    "--shadow-color": "#000000",
+     {"--message-background": "#1E60E6",
+     "--background": "#3577FC",
+     "--text-color": "#FFFFFF",
+     "--shadow-color": "#0e0e0e",
+      "name": "theme3"},
+     
+    {"--message-background": "#3E0EC2",
+    "--background": "#4E17E6",
+    "--text-color": "#FFFFFF",
+    "--shadow-color": "#0e0e0e",
      "name": "theme4"},
 
-    {"--message-background": "#690896",
-    "--background": "#6B0382",
-    "--text-color": "#CBD7F7",
-    "--shadow-color": "#210E36",
-     "name": "theme5"},
-
-    {"--message-background": "#02BF77",
-    "--background": "#97E6D8",
-    "--text-color": "#DDEDEC",
-    "--shadow-color": "#000000",
-     "name": "theme6"},
 
     {"--message-background": "#1f1f1f",
     "--background": "#2D2D2D",
     "--text-color": "#E0E0E0",
     "--shadow-color": "#000000",
-     "name": "theme7"}
+     "name": "theme5"}
 
 
 ]
